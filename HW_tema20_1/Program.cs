d739f1c84e4bba437d48ac783c62fe445a3825b8
Лилит <@LAPTOP-3EJS7JBS>
2023-07-02 10:30:53 +0300

    Добавьте файлы проекта.

---
+++ HW_tema20_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int a_1=int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int b_1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Сумма чисел равна: {Summa(a_1, b_1)}");
+int Summa(int a, int b)
+{
+    return a + b;
+}
